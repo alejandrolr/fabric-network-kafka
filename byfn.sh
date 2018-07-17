94ea8c62632311fc01bb7b71d90d318d4244acf8
Alejandro López <alejandro.lopez@kairosds.com>
2018-07-17 09:01:57 +0200

    added some wait time in byfn.sh

--- byfn.sh
+++ byfn.sh
@@ -165,7 +165,7 @@ function networkUp() {
     exit 1
   fi
   # now run the end to end script
-  sleep 5
+  sleep 10
   docker exec cli scripts/script.sh $CHANNEL_NAME $CLI_DELAY $LANGUAGE $CLI_TIMEOUT $VERBOSE
   if [ $? -ne 0 ]; then
     echo "ERROR !!!! Test failed"
